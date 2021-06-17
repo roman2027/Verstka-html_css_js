f562b50758f65c15dda57c9cb8797e19ebc4a3a7
Роман Ольшанский <rolshansky@e1r8p14.unit.ua>
2021-06-17 21:08:36 +0300

    17.06.2021 21:08

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// 1 - Иконки подбирали тут!
+// https://icomoon.io/app/#/select
+
+// 2 - При нажатии на 'Бесплатная консультация' скрипт подбирали тут!
+// http://fancyapps.com/fancybox/3/docs/#inline
+
+// 3 - Верхний слайдер подбирали тут!
+// https://kenwheeler.github.io/slick/
+
+$(function(){
+    $('.slider__inner').slick();
+});
