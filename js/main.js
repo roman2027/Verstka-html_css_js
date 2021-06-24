a6a491c0361cac8bc0e54e43d99dff89904a0468
Роман Ольшанский <rolshansky@e1r9p11.unit.ua>
2021-06-24 22:41:57 +0300

    22:41 24.06.2021

--- js/main.js
+++ js/main.js
@@ -8,5 +8,11 @@
 // https://kenwheeler.github.io/slick/
 
 $(function(){
-    $('.slider__inner').slick();
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
+
+    $('select').styler();
 });
